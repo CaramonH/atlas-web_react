522ae1aa224c3aa3399596424e5dd4bf2c3d10ac
CaramonH <caramonh@yahoo.com>
2024-04-29 11:35:48 -0500

    using webpack to bundle javascript code and manage dependencies

---
+++ task_0/src/index.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+
+const paragraphs = [
+  'Holberton Dashboard',
+  'Dashboard data for the students',
+  'Copyright - Holberton School'
+];
+
+$(document).ready(() => {
+  paragraphs.forEach(text => {
+    $('body').append(`<p>${text}</p>`);
+  });
+});
