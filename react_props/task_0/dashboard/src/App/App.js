093fd3926eb0b676b4e936f8ed1ffc6b9a59fefb
CaramonH <caramonh@yahoo.com>
2024-05-06 16:05:18 -0500

    changing notifications import, task 0 complete

--- react_props/task_0/dashboard/src/App/App.js
+++ react_props/task_0/dashboard/src/App/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import Notifications from "../Notifications/Notifications";
+import { Notifications } from "../Notifications/Notifications";
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
