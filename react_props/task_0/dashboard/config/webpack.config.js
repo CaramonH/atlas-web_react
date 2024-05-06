441d7184d512a2a2ef62cf78fe438fe6c52676b2
CaramonH <caramonh@yahoo.com>
2024-05-06 15:38:46 -0500

    moving files over from react_intro

---
+++ react_props/task_0/dashboard/config/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Entry point for the application
+  output: {
+    filename: 'bundle.js', // Output bundle file name
+    path: path.resolve(__dirname, '../dist'), // Output directory
+  },
+  devServer: {
+    port: 8000,
+    static: {
+      directory: path.join(__dirname, '../dist'), // Serve static files from the dist directory
+    },
+    compress: true,
+    hot: true,
+  },
+  devtool: 'inline-source-map', // Generate source maps for better debugging
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Match js and jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // Use babel for transpiling
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'], // Add babel presets
+          },
+        },
+      },
+      {
+        test: /\.css$/, // Match CSS files
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, // Match image files
+        use: [
+          'file-loader', // Use file-loader to emit the image file
+          {
+            loader: 'image-webpack-loader', // Use image-webpack-loader for images
+          },
+        ],
+      },
+    ],
+  },
+};
