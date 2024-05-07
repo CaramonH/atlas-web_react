4ac141cd7a45f7139168d355e84e1b5b65f93ab1
CaramonH <caramonh@yahoo.com>
2024-05-07 11:55:53 -0500

    added testing for rendering

--- react_props/task_4/dashboard/src/App/App.test.js
+++ react_props/task_4/dashboard/src/App/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
-import Notifications from "../Notifications/Notifications";
+import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
 
 describe('App', () => {
   let wrapper;
@@ -31,5 +32,24 @@ describe('App', () => {
 
   it('contains the Footer component', () => {
     expect(wrapper.contains(<Footer />)).toBe(true);
+  });
+
+  it('does not render CourseList when isLoggedIn is false', () => {
+    const wrapper = shallow(<App isLoggedIn={false} />);
+    expect(wrapper.find(CourseList).length).toBe(0);
+  });
+
+  describe('when isLoggedIn is true', () => {
+    beforeEach(() => {
+      wrapper.setProps({ isLoggedIn: true });
+    });
+
+    it('does not render Login component', () => {
+      expect(wrapper.find(Login).length).toBe(0);
+    });
+
+    it('renders CourseList component', () => {
+      expect(wrapper.find(CourseList).length).toBe(1);
+    });
   });
 });
