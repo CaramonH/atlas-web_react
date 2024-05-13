39c9e82ffb98ca6fc21a4284a31271c64f4dadee
CaramonH <caramonh@yahoo.com>
2024-05-13 15:30:03 -0500

    added aphrodite inline styling

--- React_inline_styling/task_1/dashboard/src/App/App.js
+++ React_inline_styling/task_1/dashboard/src/App/App.js
@@ -8,6 +8,7 @@ import CourseList from "../CourseList/CourseList";
 import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
 import BodySection from "../BodySection/BodySection";
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
 class App extends Component {
   constructor(props) {
@@ -46,20 +47,53 @@ class App extends Component {
     const { isLoggedIn } = this.props;
     const { listCourses, listNotifications } = this.state;
 
+    const styles = StyleSheet.create({
+      body: {
+        textAlign: 'center',
+      },
+
+      headerWrapper: {
+        display: 'flex',
+        justifyContent:'space-between',
+        alignItems: 'flex-start',
+        borderBottom: '5px solid #00003C',
+      },
+
+      headerNotifications: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+      },
+
+      footer: {
+        fontFamily: "'Galano Grotesque Alt', sans-serif",
+        fontStyle: 'italic',
+        fontSize: '1.1rem',
+        borderTop: '5px solid #00003C',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: '0',
+      },
+    });
+
+
     return (
       <>
-        <div className="App-header-wrapper">
+        <div className={css(styles.headerWrapper)}>
           <Header />
-          <div className="App-menu-notifications">
+          <div className={css(styles.headerNotifications)}>
             <Notifications listNotifications={listNotifications} />
           </div>
         </div>
-        <div className="App">
+        <div className={css(styles.body)}>
           {isLoggedIn ? (
             <BodySectionWithMarginBottom title='Course List'>
               <CourseList listCourses={listCourses} />
             </BodySectionWithMarginBottom>
-            ) : (
+          ) : (
             <BodySectionWithMarginBottom title='Log in to continue'>
               <Login />
             </BodySectionWithMarginBottom>
@@ -67,7 +101,9 @@ class App extends Component {
           <BodySection title='News from the School'>
             <p>Caramon is number one!!</p>
           </BodySection>
-          <Footer />
+        </div>
+        <div>
+          <Footer footerClassName={css(styles.footer)} />
         </div>
       </>
     );
