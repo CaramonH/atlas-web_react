628d3af71bf00be84b34cb82439f3079e5938876
CaramonH <caramonh@yahoo.com>
2024-05-14 12:19:32 -0500

    update margins

--- React_inline_styling/task_3/dashboard/src/Notifications/Notifications.js
+++ React_inline_styling/task_3/dashboard/src/Notifications/Notifications.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import NotificationItem from "./NotificationItem";
 import { NotificationItemShape } from "./NotificationItemShape";
-import closeIcon from '../assets/close-icon.png';
+import closeIcon from '../assets/close.png';
 import PropTypes from 'prop-types';
 import { StyleSheet, css } from 'aphrodite';
 
