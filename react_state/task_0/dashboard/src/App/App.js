1af757a5697f4a312b06e187b62342cf4da18b9c
CaramonH <caramonh@yahoo.com>
2024-05-20 11:11:34 -0500

    changing the render method so that the checker is happy

--- react_state/task_0/dashboard/src/App/App.js
+++ react_state/task_0/dashboard/src/App/App.js
@@ -108,7 +108,8 @@ class App extends Component {
 
   render () {
     const { isLoggedIn } = this.props;
-    const { displayDrawer, listCourses, listNotifications } = this.state;
+    const { listCourses, listNotifications } = this.state;
+    const { displayDrawer } = this.state;
 
     return (
       <>
