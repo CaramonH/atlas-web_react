e9280e472f1515e506b594ff4a9d160fe6d65618
CaramonH <caramonh@yahoo.com>
2024-05-21 14:18:15 -0500

    Set up Normalizr schema and add tests for notifications

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,8 +1,20 @@
-// Import the JSON data
+import { normalize, schema } from 'normalizr';
 import * as notificationsData from '../../notifications.json';
+
+// Define entities
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message
+});
 
 // Function to get all notifications by user ID
 export function getAllNotificationsByUser(userId) {
-  return notificationsData.default.filter(notification => notification.author.id === userId)
-                                  .map(notification => notification.context);
+  const normalizedData = normalize(notificationsData.default, [notification]);
+  const userNotifications = normalizedData.entities.notifications
+    .filter(notification => notification.author === userId)
+    .map(notification => notification.context);
+
+  return userNotifications.map(messageId => normalizedData.entities.messages[messageId]);
 }
