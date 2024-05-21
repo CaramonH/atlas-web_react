0041c6879cbd18ba2a9a3ed29d03b8962d582249
CaramonH <caramonh@yahoo.com>
2024-05-21 14:58:31 -0500

    oops, forgot action types

--- 0x08_react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionTypes.js
+++ 0x08_react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionTypes.js
@@ -1,5 +1,10 @@
 //notificationActionTypes.js
 
+// Define action types
+export const MARK_AS_READ = 'MARK_AS_READ';
+export const SET_TYPE_FILTER = 'SET_TYPE_FILTER';
+
+// Define filter states
 export const NotificationTypeFilters = {
   DEFAULT: 'DEFAULT',
   URGENT: 'URGENT'
