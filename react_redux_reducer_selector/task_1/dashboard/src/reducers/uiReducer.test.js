7e36fb9c713211ff506b046d5a744752a9bd88fd
CaramonH <caramonh@yahoo.com>
2024-05-29 12:45:00 -0500

    update tests for the immutable objects

--- react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
+++ react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -1,30 +1,28 @@
+import { Map } from 'immutable';
 import uiReducer from './uiReducer';
 import { DISPLAY_NOTIFICATION_DRAWER } from '../actions/uiActionTypes';
 
 describe('uiReducer', () => {
-  const initialState = {
+  const initialState = Map({
     isNotificationDrawerVisible: false,
     isUserLoggedIn: false,
-    user: {}
-  };
+    user: Map({})
+  });
 
   it('should return the initial state when no action is passed', () => {
     const newState = uiReducer(undefined, {});
-    expect(newState).toEqual(initialState);
+    expect(newState.toJS()).toEqual(initialState.toJS());
   });
 
   it('should return the initial state when the action SELECT_COURSE is passed', () => {
     const newState = uiReducer(undefined, { type: 'SELECT_COURSE' });
-    expect(newState).toEqual(initialState);
+    expect(newState.toJS()).toEqual(initialState.toJS());
   });
 
   it('should change isNotificationDrawerVisible to true when DISPLAY_NOTIFICATION_DRAWER is passed', () => {
     const action = { type: DISPLAY_NOTIFICATION_DRAWER };
-    const expectedState = {
-      ...initialState,
-      isNotificationDrawerVisible: true
-    };
+    const expectedState = initialState.set('isNotificationDrawerVisible', true);
     const newState = uiReducer(initialState, action);
-    expect(newState).toEqual(expectedState);
+    expect(newState.toJS()).toEqual(expectedState.toJS());
   });
 });
