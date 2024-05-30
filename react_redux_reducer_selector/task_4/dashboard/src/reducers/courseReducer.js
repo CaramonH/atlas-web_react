ce83f7fa386d62554a1e24ae058e1f7ab0772c0c
CaramonH <caramonh@yahoo.com>
2024-05-30 10:45:07 -0500

    trying to get tests to pass

--- react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.js
+++ react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.js
@@ -1,6 +1,6 @@
-import { Map } from 'immutable';
+import { Map, fromJS } from 'immutable';
 import { SELECT_COURSE, UNSELECT_COURSE, FETCH_COURSE_SUCCESS } from "../actions/courseActionTypes";
-import { courseNormalizer } from "../schema/courses";
+import { coursesNormalizer } from '../schema/courses';
 
 const initialState = Map({
   courses: Map(),
@@ -9,17 +9,18 @@ const initialState = Map({
 const courseReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_COURSE_SUCCESS:
-      // Normalize the data and merge it with the state
-      const normalizedData = courseNormalizer(action.data);
-      return state.mergeIn(['courses'], normalizedData.entities.courses);
+      // Normalize the data and ensure it is converted to an immutable Map
+      const normalizedData = coursesNormalizer(action.data);
+      const coursesWithSelection = fromJS(normalizedData.entities.courses).map(course => course.set('isSelected', false));
+      return state.mergeIn(['courses'], coursesWithSelection);
 
     case SELECT_COURSE:
-      // Update the value of the item
-      return state.setIn(['courses', action.index, 'isSelected'], true);
+      // Ensure action.index is treated as a string for consistency
+      return state.setIn(['courses', String(action.index), 'isSelected'], true);
 
     case UNSELECT_COURSE:
-      // Update the value of the item
-      return state.setIn(['courses', action.index, 'isSelected'], false);
+      // Ensure action.index is treated as a string for consistency
+      return state.setIn(['courses', String(action.index), 'isSelected'], false);
 
     default:
       return state;
--- react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.test.js
+++ react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.test.js
@@ -1,64 +1,47 @@
-import courseReducer from "./courseReducer";
-import { SELECT_COURSE, UNSELECT_COURSE, FETCH_COURSE_SUCCESS } from "../actions/courseActionTypes";
-import { courseNormalizer } from "../schema/courses";
-import Immutable from 'immutable';
+import courseReducer from './courseReducer';
+import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from '../actions/courseActionTypes';
+import { fromJS } from 'immutable';
 
 describe('courseReducer', () => {
-  it('should return initial state when no action is passed', () => {
-    const state = courseReducer(undefined, {});
-    expect(state).toEqual(Immutable.Map({
-      courses: Immutable.Map({})
-    }));
-  });
-
   it('should handle FETCH_COURSE_SUCCESS action', () => {
     const courses = [
       { id: 1, name: 'ES6', credit: 60 },
-      { id: 2, name: "Webpack", credit: 20 },
-      { id: 3, name: "React", credit: 40 }
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 }
     ];
-    const action = {
-      type: 'FETCH_COURSE_SUCCESS',
-      data: courses
-    };
+    const action = { type: FETCH_COURSE_SUCCESS, data: courses };
     const state = courseReducer(undefined, action);
-    const normalizedCourses = courseNormalizer(courses);
-    expect(state.courses).toEqual(normalizedCourses.entities.courses);
+    const expectedState = fromJS({
+      courses: {
+        1: { id: 1, name: 'ES6', credit: 60, isSelected: false },
+        2: { id: 2, name: 'Webpack', credit: 20, isSelected: false },
+        3: { id: 3, name: 'React', credit: 40, isSelected: false }
+      }
+    });
+    expect(state).toEqual(expectedState);
   });
 
   it('should handle SELECT_COURSE action', () => {
-    const courses = [
-      { id: 1, name: 'ES6', credit: 60 },
-      { id: 2, name: "Webpack", credit: 20 },
-      { id: 3, name: "React", credit: 40 }
-    ];
-    const normalizedCourses = courseNormalizer(courses);
-    const initialState = {
-      courses: normalizedCourses.entities.courses
-    };
-    const action = {
-      type: SELECT_COURSE,
-      index: '2'
-    };
+    const initialState = fromJS({
+      courses: {
+        1: { id: 1, name: 'ES6', credit: 60, isSelected: false },
+        2: { id: 2, name: 'Webpack', credit: 20, isSelected: false }
+      }
+    });
+    const action = { type: SELECT_COURSE, index: 1 };
     const state = courseReducer(initialState, action);
-    expect(state.courses['2'].isSelected).toBe(true);
+    expect(state.getIn(['courses', '1', 'isSelected'])).toBe(true);
   });
 
   it('should handle UNSELECT_COURSE action', () => {
-    const courses = [
-      { id: 1, name: 'ES6', credit: 60 },
-      { id: 2, name: "Webpack", credit: 20, isSelected: true },
-      { id: 3, name: "React", credit: 40 }
-    ];
-    const normalizedCourses = courseNormalizer(courses);
-    const initialState = {
-      courses: normalizedCourses.entities.courses
-    };
-    const action = {
-      type: UNSELECT_COURSE,
-      index: '2'
-    };
+    const initialState = fromJS({
+      courses: {
+        1: { id: 1, name: 'ES6', credit: 60, isSelected: true },
+        2: { id: 2, name: 'Webpack', credit: 20, isSelected: false }
+      }
+    });
+    const action = { type: UNSELECT_COURSE, index: 1 };
     const state = courseReducer(initialState, action);
-    expect(state.courses['2'].isSelected).toBe(false);
+    expect(state.getIn(['courses', '1', 'isSelected'])).toBe(false);
   });
 });
