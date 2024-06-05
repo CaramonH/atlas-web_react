204b571974e9fcf51758316327e1b18caf5a6f00
CaramonH <caramonh@yahoo.com>
2024-06-05 15:23:47 -0500

    updating index

--- react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App/App';
 import { configureStore } from '@reduxjs/toolkit';
-import uiReducer from './reducers/uiReducer';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // Create Redux store holding state of your app
 const store = configureStore({
@@ -14,6 +14,7 @@ const store = configureStore({
     getDefaultMiddleware({
       serializableCheck: false,
     }),
+  enhancers: [composeWithDevTools()], // Add composeWithDevTools to enable Redux DevTools extension
 });
 
 // Root for the main app
